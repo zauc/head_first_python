676e17ac049105156fcdd087eda13a6fcce950b2
Timo <greycd@gmail.com>
2014-03-10 21:29:21 +0800

    chapter4 More test than chapter3

---
+++ chapter4/page107.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+man=[]
+other=[]
+try:
+	data = open('sketch.txt')
+	for each_line in data:
+		try:
+			(role, line_spoken) = each_line.split(':', 1)
+			line_spoken = line_spoken.strip()
+			if role == 'Man':
+				man.append(line_spoken)
+			elif role == 'Other Man':
+				other.append(line_spoken)
+		except ValueError:
+			pass
+	data.close()
+except IOError:
+	print('The datafile is missiong!')
+
+print(man)
+print(other)
